2557a6ced7085e96eb28a6f09715964b1b9baf25
Ashle Johnson <ashlerose101@gmail.com>
2024-11-24 15:37:52 -0500

    Edited world.css, world.js and world.php

---
+++ world.js
@@ -0,0 +1,45 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+    
+
+
+function Lookupbtn (){
+
+    let country = document.getElementById('country').value.trim();
+    let url = 'http://localhost/info2180-lab5/world.php?=country${(country)}';
+    fetch(url)
+    .then(response => response.text())
+    .then(data=> {
+        console.log(data)
+        result.innerHTML = data
+    })
+    .catch(error => {
+        alert(error)
+    });
+
+
+
+          
+
+
+
+
+
+}
+
+document.getElementById("lookup").addEventListener("click", Lookupbtn);
+
+
+
+
+
+
+
+
+
+
+});
