c9c3fa68167238a7402fe06eb9702d47471938b5
Ashle Johnson <ashlerose101@gmail.com>
2024-12-01 19:59:12 -0500

    Edited index.html, world.css, world.js and world.php

--- world.php
+++ world.php
@@ -4,19 +4,36 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $country = $_GET['country'];
+//$cities = $_GET['cities'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 //$stmt = $conn->query("SELECT * FROM countries");
+
+
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
+
+
 ?>
-<ul>
+
+<table>
+
+<tr>
+					<th>Country Name</th>
+					<th>Continent</th>
+					<th>Independence Year</th>
+					<th>Head of State</th>
+</tr>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+<tr>
+  <td><?= $row['name']; ?></td>
+  <td><?= $row['continent'];?></td>
+  <td><?= $row['independence_year'];?></td>
+  <td><?= $row['head_of_state'];?></td>
+</tr>
 <?php endforeach; ?>
-</ul> 
 
-
-
+</table> 
